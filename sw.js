a8bae15d2c4b781c68063f2cc849734356a4df87
Rodrigo <condorirodrigo311@gmail.com>
2024-02-05 13:10:58 -0400

    Guardé mis cambios

--- sw.js
+++ sw.js
@@ -22,3 +22,10 @@ self.addEventListener('install', function(event) {
       })
     );
   });
+
+  document.addEventListener("DOMContentLoaded", function() {
+    // Intenta bloquear la orientación de la pantalla a 'portrait'
+    window.screen.orientation.lock('portrait').catch(function(error) {
+        console.log("Error al bloquear la orientación de la pantalla: " + error);
+    });
+});
